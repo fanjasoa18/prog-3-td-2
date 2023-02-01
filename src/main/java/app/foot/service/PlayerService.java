5ee9c76fd4d11ef553e8cc29057188bdfbe33c4d
fanjasoa <fanjasoa18@gmail.com>
2023-02-01 23:43:41 +0300

    feat: PlayerSerice

--- src/main/java/app/foot/service/PlayerService.java
+++ src/main/java/app/foot/service/PlayerService.java
@@ -2,6 +2,7 @@ package app.foot.service;
 
 import app.foot.model.Player;
 import app.foot.repository.PlayerRepository;
+import app.foot.repository.entity.PlayerEntity;
 import app.foot.repository.mapper.PlayerMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -28,4 +29,13 @@ public class PlayerService {
                 .map(mapper::toDomain)
                 .collect(Collectors.toUnmodifiableList());
     }
+
+    public List<Player> updatePlayers(List<Player> toUpdate) {
+        return repository.saveAll(toUpdate.stream()
+                        .map(mapper::toEntity)
+                        .collect(Collectors.toUnmodifiableList())).stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
 }
