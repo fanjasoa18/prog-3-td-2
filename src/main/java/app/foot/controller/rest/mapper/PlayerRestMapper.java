a44346a485b58c1ec96ffec3f18bb84598acb76a
fanjasoa <fanjasoa18@gmail.com>
2023-02-09 12:08:59 +0300

    fix: update playerRestMapper

--- src/main/java/app/foot/controller/rest/mapper/PlayerRestMapper.java
+++ src/main/java/app/foot/controller/rest/mapper/PlayerRestMapper.java
@@ -1,6 +1,8 @@
 package app.foot.controller.rest.mapper;
 
+import app.foot.controller.response.UpdatePlayerResponse;
 import app.foot.controller.rest.Player;
+import app.foot.repository.entity.PlayerEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +24,14 @@ public class PlayerRestMapper {
                 .isGuardian(rest.getIsGuardian())
                 .name(rest.getName())
                 .teamName(rest.getTeamName())
+                .build();
+    }
+
+    public app.foot.model.Player toEntity(UpdatePlayerResponse rest) {
+        return app.foot.model.Player.builder()
+                .id(rest.getId())
+                .name(rest.getName())
+                .isGuardian(rest.isGuardian())
                 .build();
     }
 }
